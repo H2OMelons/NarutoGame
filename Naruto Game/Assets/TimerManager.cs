6dd8c93bc659b4a622ee5d3d93be911f207fe75a
Rycintech <rycintech@gmail.com>
2018-06-23 21:39:58 -0700

    Bug Fix
    Fixed a bug where the character selections were matched with the wrong character name when the game started

---
+++ Naruto Game/Assets/TimerManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerManager : MonoBehaviour {
+
+    private const int START_TIME = 99;
+    private int startTime;
+
+    public GameObject timer;
+
+	// Use this for initialization
+	void Start () {
+        //this.enabled = false;
+        startTime = (int)Time.time;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        int currTime = ((int)Time.time) - startTime;
+        string currTimeStr = currTime.ToString();
+        timer.GetComponent<Text>().text = currTimeStr;
+	}
+
+    public void StartTimer()
+    {
+        startTime = (int)Time.time;
+        this.enabled = true;
+    }
+
+    public void EndTimer()
+    {
+        this.enabled = false;
+    }
+
+    public void RestartTimer()
+    {
+
+    }
+
+    private void SetTimer(int time)
+    {
+
+    }
+}
